d3582cf037c2bcb12b4b0ef696071e4a8549c38e
Dev-EduardoMelo <deveduardomelo@gmail.com>
2021-05-17 16:28:27 -0300

    ajuste do email

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    foneNumber: {type: String, required: true},
-    city: {type: String, required: true},
-    bairro: {type: String, required: true},
-    rua: {type: String, required: true},
-    number: {type: String, required: true},
-    createdAt: {type: Date, default: Date.now}
+    name: { type: String, required: true },
+    email: { type: String, required: false },
+    foneNumber: { type: String, required: true },
+    city: { type: String, required: true },
+    bairro: { type: String, required: true },
+    rua: { type: String, required: true },
+    number: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 
 })
 
